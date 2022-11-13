c40d4d4dd5419926eb8c1425c263fe384715f530
sumit6675 <sumitchimkar0@gmail.com>
2022-11-13 16:17:57 +0530

    adding new files

--- my-app/src/Components/Productpage.jsx
+++ my-app/src/Components/Productpage.jsx
@@ -1,4 +1,4 @@
-import { Grid, Heading } from "@chakra-ui/react";
+import { Grid, Heading, Skeleton } from "@chakra-ui/react";
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -6,14 +6,54 @@ import Productonepage from "./Productonepage";
 
 function Productpage() {
   const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
+    setLoading(true);
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
-      .then((res) => setData(res));
+      .then((res) => {
+        setData(res);
+        setLoading(false);
+      });
   }, []);
-  const handleCart=(i)=>{
-    console.log(i)
+  const handleCart = (i) => {
+    console.log(i);
+  };
+
+  if (loading) {
+    return (
+      <>
+        <Navbar />
+        <Heading
+          fontFamily={"Brush Script MT, Brush Script Std, cursive"}
+          my="55px"
+          textAlign={"center"}
+        >
+          Product Page
+        </Heading>
+        <Grid w="90%" m="auto" templateColumns={"repeat(4, 1fr)"} gap="20px">
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+          <Skeleton height="220px" />
+        </Grid>
+        <Footer />
+      </>
+    );
   }
+
   return (
     <div>
       <Navbar />
@@ -25,16 +65,21 @@ function Productpage() {
         Product Page
       </Heading>
       <Grid w="90%" m="auto" templateColumns={"repeat(4, 1fr)"} gap="20px">
-       {data.map((i)=>{
-        return(
-            <Productonepage image={i.image} alt={i.id} 
-            title={i.title} price={i.price} 
-            category={i.category} rate={i.rating.rate} 
-            count={i.rating.count} handleClick={()=>handleCart(i)} 
-            key={i.id}
+        {data.map((i) => {
+          return (
+            <Productonepage
+              image={i.image}
+              alt={i.id}
+              title={i.title}
+              price={i.price}
+              category={i.category}
+              rate={i.rating.rate}
+              count={i.rating.count}
+              handleClick={() => handleCart(i)}
+              key={i.id}
             />
-        )
-       })}
+          );
+        })}
       </Grid>
       <Footer />
     </div>
